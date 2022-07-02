bc27e6e49b343197bc3590df3ae5010db7258364
reed <reed.mcfadden@gmail.com>
2022-07-02 16:41:22 -0500

    added temperature update to file every 2 seconds

--- src/simply-scrape/login.py
+++ src/simply-scrape/login.py
@@ -4,6 +4,7 @@ Created on Jul 1, 2022
 @author: reed
 '''
 
+from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.keys import Keys
@@ -33,17 +34,26 @@ def click_home_button(driver):
 
 def get_world_temperature(driver):
     """ Get the temperature from the text, contained in the last characters of the string """
-    time.sleep(2)
     element = driver.find_element(by="xpath", value='//*[@id="displaytimer"]')
     world_temp = element.text.split(": ")[1]
     return world_temp 
+
+
+def write_new_temperature(driver):
+    with open("temperatures.txt", "a") as temperature_file:
+        new_entry = str(datetime.now()) + " -- Temperature: " + get_world_temperature(driver) + "\n"
+        temperature_file.write(new_entry)
 
 
 def main():
     driver = get_driver()
     login(driver)
     click_home_button(driver)
-    print(get_world_temperature(driver))
+    
+    # write each updated temperature to a file. check every 2 seconds.
+    while True:
+        time.sleep(2)
+        write_new_temperature(driver)
 
 
 if __name__ == '__main__':
