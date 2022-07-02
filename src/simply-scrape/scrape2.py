86e9ea0c9acc8b7b6b92cf29a6ce9cb3f8da5782
reed <reed.mcfadden@gmail.com>
2022-07-02 16:05:32 -0500

    moved logic to get world temp to one function

--- src/simply-scrape/scrape2.py
+++ src/simply-scrape/scrape2.py
@@ -17,17 +17,17 @@ def get_driver():
     return driver
 
 
-def get_text_temp(text):
+def get_world_temperature(driver):
     """ Get the temperature from the text, contained in the last characters of the string """
-    output = text.split(": ")[1]
-    return output
+    time.sleep(2)
+    element = driver.find_element(by="xpath", value='//*[@id="displaytimer"]')
+    world_temp = element.text.split(": ")[1]
+    return world_temp 
 
 
 def main():
     driver = get_driver()
-    time.sleep(2)
-    element = driver.find_element(by="xpath", value='//*[@id="displaytimer"]')
-    return get_text_temp(element.text)
+    return get_world_temperature(driver)
 
 
 if __name__ == '__main__':
